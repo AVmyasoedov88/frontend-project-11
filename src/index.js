6ae27e91054225658ed730675a49c51222cd1e76
AVmyasoedov88 <antonmyasoedov88@gmail.com>
2023-06-11 17:55:50 +0300

    fix step 1

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import './styles.scss';
 import 'bootstrap';
+// https://github.com/k1ntsugi1/frontend-project-lvl3/blob/main/dist/main.6adef979aa645ea90fbf.js
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ const config = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
   },
   devServer: {
     open: true,
