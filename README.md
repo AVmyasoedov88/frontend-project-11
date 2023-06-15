a9e3708ac0cb1bb12e20e750cc1d49390fa6313b
AVmyasoedov88 <antonmyasoedov88@gmail.com>
2023-06-15 22:31:06 +0300

    15/06/2023

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 [![github action status](https://github.com/hexlet-boilerplates/webpack-package/workflows/Node%20CI/badge.svg)](https://github.com/hexlet-boilerplates/webpack-package/actions)
 [![Code Climate](https://codeclimate.com/github/hexlet-boilerplates/webpack-package/badges/gpa.svg)](https://codeclimate.com/github/hexlet-boilerplates/webpack-package)
 
+[![Maintainability](https://api.codeclimate.com/v1/badges/0b92955db78000d6a42a/maintainability)](https://codeclimate.com/github/AVmyasoedov88/frontend-project-11/maintainability)
+
+[![Test Coverage](https://api.codeclimate.com/v1/badges/0b92955db78000d6a42a/test_coverage)](https://codeclimate.com/github/AVmyasoedov88/frontend-project-11/test_coverage)
+
 ## Setup
 
 ```sh
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
-  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  
+  //mode: process.env.NODE_ENV || 'development',
   module: {
     rules: [
       {
@@ -29,12 +36,14 @@ export default {
       },
     ],
   },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'template.html',
+      template: 'index.html',
     }),
   ],
-  output: {
-    clean: true,
-  },
+  mode: 'production'
 };
