89cca0db29f902fa55b4cb86bfb8f3259baa003c
AVmyasoedov88 <antonmyasoedov88@gmail.com>
2023-07-10 21:09:10 +0300

    add gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,6 @@ const __dirname = path.dirname(__filename);
 export default {
   entry: './src/index.js',
 
-  // eslint-disable-next-line no-undef
   mode: process.env.NODE_ENV || 'development',
   module: {
     rules: [
