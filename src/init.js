fdc9a4b04282f9ecde67514ecf1170843cfcc753
AVmyasoedov88 <antonmyasoedov88@gmail.com>
2023-07-22 21:12:08 +0300

    fix4

--- src/init.js
+++ src/init.js
@@ -67,12 +67,9 @@ const init = async () => {
         .then((response) => {
           const parserData = parser(response);
           const [, topics] = parserData;
-          topics.forEach((topic) => {
-            return (topic.id = _.uniqueId());
-          });
+          topics.forEach((topic) => (topic.id = _.uniqueId()));
           const oldTopics = statE.content.topics.map((topic) =>
-            topic.map((item) => item.title),
-          );
+            topic.map((item) => item.title));
           const flatOldTopics = _.flatten(oldTopics);
           const newTopicS = topics.filter(
             (topic) => !flatOldTopics.includes(topic.title),
@@ -97,8 +94,7 @@ const init = async () => {
       setTimeout(
         () => updatePosts(state, i18nextInstance, watchedStateRsS),
         5000,
-      ),
-    );
+      ));
   };
   const watchedStateRsS = watchedStateRss(state, i18nextInstance, elements);
 
@@ -119,9 +115,7 @@ const init = async () => {
       .then((response) => {
         const result = parser(response);
         const [feeds, topics] = result;
-        topics.forEach((topic) => {
-          return topic.id = _.uniqueId();
-        });
+        topics.forEach((topic) => (topic.id = _.uniqueId()));
         feeds.url = url;
         watchedStateRsS.content.feeds.push(feeds);
         watchedStateRsS.content.topics.push(topics);
